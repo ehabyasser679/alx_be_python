ea821a014fbe9186b5b084563f12c58a657ed272
Ehab Yasser <ehabyasser679@gmail.com>
2025-12-17 18:47:08 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,7 +12,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 raise ZeroDivisionError("Cannot divide by zero")
-            else:
+            elif num2 != 0:
                 return num1 / num2
         case _:
             raise ValueError("Invalid operation")
+
