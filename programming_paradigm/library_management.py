59773aff4a48c2bc3a8b6b3965fe8e98a307f8e5
Ehab Yasser <ehabyasser679@gmail.com>
2025-12-23 23:10:40 +0200

    Add files via upload

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+        def check_out(self):
+            if not self._is_checked_out:
+                self._is_checked_out = True
+                return True
+            return False
+        
+class Library:
+    def __init__(self, books):
+        self._books = books
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                return book.check_out()
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book._is_checked_out = False
+                return True
+        return False
+
+    def display_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author} - {'Checked out' if book._is_checked_out else 'Available'}")
+
+    def list_available_books(self):
+        return [book for book in self._books if not book._is_checked_out]
