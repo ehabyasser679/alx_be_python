a36c60090a65a656c13b82e030219f36da3989cc
Ehab Yasser <ehabyasser679@gmail.com>
2025-12-23 22:15:44 +0200

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3),2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4,2),8)
     
     def test_divide(self):
