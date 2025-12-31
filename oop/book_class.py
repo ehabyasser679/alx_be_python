9ca8089cd43080d16ad7fe0adf1f3b470eaffc00
Ehab Yasser <ehabyasser679@gmail.com>
2025-12-31 21:42:02 +0200

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,7 +5,7 @@ class Book:
         self.year = year
 
     def __str__(self):
-        return f"title {self.title}", f"by {self.author}", "published in {self.year}"
+        return f"title {self.title} by {self.author} published in {self.year}"
     
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
